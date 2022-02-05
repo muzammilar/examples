162d3f66a767c8e05bf950544f939a83db95a4cb
Muzammil Abdul Rehman <muzammil.abdul.rehman@gmail.com>
2022-02-04 20:56:52 -0800

    [MNR] clickhouse - add zookeeper queries

--- clickhouse/single-server-with-keeper/sql/4-results.sql
+++ clickhouse/single-server-with-keeper/sql/4-results.sql
@@ -19,4 +19,19 @@ SELECT * FROM system.zookeeper WHERE path='/clickhouse/tables/test-test_table_lo
 
 SELECT * FROM system.zookeeper WHERE path='/clickhouse/tables/test-test_table_local/0001';
 
+SELECT * FROM system.zookeeper WHERE path='/clickhouse/tables/test-test_table_local/0001/replicas';
+
+SELECT * FROM system.zookeeper WHERE path='/clickhouse/tables/test-test_table_local/0001/replicas/clickhouse-server-01';
+
+SELECT * FROM system.zookeeper WHERE path='/clickhouse/tables/test-test_table_local/0001/replicas/clickhouse-server-01/parts';
+
+SELECT * FROM system.zookeeper WHERE path='/clickhouse/tables/test-test_table_local/0001/replicas/clickhouse-server-01/queue';
+
+SELECT * FROM system.zookeeper WHERE path='/clickhouse/tables/test-test_table_local/0001/replicas/clickhouse-server-01/is_active';
+
+SELECT * FROM system.zookeeper WHERE path='/clickhouse/tables/test-test_table_local/0001/replicas/clickhouse-server-01/is_lost';
+
+/* Dropping Dead Replica: https://clickhouse.com/docs/en/sql-reference/statements/system/#query_language-system-drop-replica */
+/* Live replicas use `DROP TABLE` */
+
 ;;
