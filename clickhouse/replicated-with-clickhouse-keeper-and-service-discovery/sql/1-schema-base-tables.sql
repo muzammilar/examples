15d2af69b3839789b21add69f750eac91b76cd2a
Muzammil Abdul Rehman <muzammil.abdul.rehman@gmail.com>
2022-11-07 07:18:58 -0800

    [FTR] Draft: ClickHouse Cluster Auto Discovery Example

---
+++ clickhouse/replicated-with-clickhouse-keeper-and-service-discovery/sql/1-schema-base-tables.sql
@@ -0,0 +1,74 @@
+/* Decrease DDL timeout to 5 seconds in case some server is down. */
+SET distributed_ddl_task_timeout = 5;
+
+/* Create a database for storage table */
+CREATE DATABASE IF NOT EXISTS test;
+
+/* Create a separate database for Materialized Views in case you want to recreate them */
+CREATE DATABASE IF NOT EXISTS test_mvs;
+
+/* Create a database for views (for search) */
+CREATE DATABASE IF NOT EXISTS test_views;
+
+/* Create a local storage table */
+/* Note: using `{table}` in Replicated can cause issues when doing ALTER RENAME on the table since it will expand to different path */
+CREATE TABLE IF NOT EXISTS test.test_table_local
+(
+    `insert_timestamp` DateTime,
+    `val` UInt64
+)
+ENGINE = ReplicatedMergeTree
+PARTITION BY toYearWeek(insert_timestamp)
+ORDER BY insert_timestamp
+TTL insert_timestamp + toIntervalDay(120);
+
+/* Create a SummingMergeTree (SMT) for hourly data */
+CREATE TABLE IF NOT EXISTS test.test_table_hourly_smt_local
+(
+    `insert_timestamp` DateTime,
+    `val` UInt64,
+    `total` UInt64
+)
+ENGINE = ReplicatedSummingMergeTree
+PARTITION BY toYearWeek(insert_timestamp)
+ORDER BY (insert_timestamp, val)
+TTL insert_timestamp + toIntervalDay(120);
+
+/* Create a Materialized View from the base ingest table to the SMT */
+CREATE MATERIALIZED VIEW IF NOT EXISTS test_mvs.test_table_hourly_smt_mv_local TO test.test_table_hourly_smt_local AS
+SELECT
+    toStartOfHour(insert_timestamp) AS insert_timestamp,
+    val,
+    count() as total
+FROM test.test_table_local
+GROUP BY
+    insert_timestamp,
+    val;
+
+/* Create an Unreplicated SummingMergeTree (SMT) for hourly data */
+/* Even though the table itself is unreplicated, we use the existing cluster (which has configs for replication but they don't affect DDLs) */
+CREATE TABLE IF NOT EXISTS test.test_table_hourly_unreplicated_smt_local
+(
+    `insert_timestamp` DateTime,
+    `val` UInt64,
+    `total` UInt64
+)
+ENGINE = SummingMergeTree()
+PARTITION BY toYearWeek(insert_timestamp)
+ORDER BY (insert_timestamp, val)
+TTL insert_timestamp + toIntervalDay(120);
+
+/* Create a Materialized View from the base ingest table to the Unreplicated SMT */
+CREATE MATERIALIZED VIEW IF NOT EXISTS test_mvs.test_table_hourly_unreplicated_smt_mv_local TO test.test_table_hourly_unreplicated_smt_local AS
+SELECT
+    toStartOfHour(insert_timestamp) AS insert_timestamp,
+    val,
+    count() as total
+FROM test.test_table_local
+GROUP BY
+    insert_timestamp,
+    val;
+
+
+
+;;
