2363b0fd32b39d99e4be9477669fd8a719817b1b
Muzammil Abdul Rehman <muzammil.abdul.rehman@gmail.com>
2024-05-09 22:59:12 -0700

    [VAGRANT] Adding a PostrgreSQL VMs Example (#7)

---
+++ vagrant/multi-vm-postgres/scripts/postgres_repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# source: https://www.postgresql.org/download/linux/ubuntu/
+
+set -xe
+
+# Create the file repository configuration:
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+# Import the repository signing key:
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+
+# Update the package lists:
+apt update
