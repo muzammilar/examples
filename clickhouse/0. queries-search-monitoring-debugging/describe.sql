aabb17ca6fbf29b388622332ec3521f0ce52ae71
Muzammil Abdul Rehman <muzammil.abdul.rehman@gmail.com>
2025-09-11 18:49:11 -0700

    [CLICKHOUSE] Describe Queries

---
+++ clickhouse/0. queries-search-monitoring-debugging/describe.sql
@@ -0,0 +1,4 @@
+/* Describe a URL. Multiple other formats are also supported */
+DESCRIBE url('https://clickhouse-public-datasets.s3.eu-central-1.amazonaws.com/hits.avro', Avro)
+/* Describe a file in a specifc format. Multiple other formats are also supported */
+DESCRIBE url('my_file.avro', Avro)
